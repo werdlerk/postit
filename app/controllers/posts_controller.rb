6a932bff0291fec902ebbaf7472bd8bea1bfac7f
Koen Werdler <koen@morgenwerk.nl>
2014-05-29 23:19:41 +0200

    Implementation for editing and updating posts. Improved index look & feel.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,41 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = User.find(1)
 
+    if @post.save
+      flash[:notice] = "Your post has been created."
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params)
+      flash[:notice] = "Your post has been updated."
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+
+  def post_params
+    # require(..).permit! to permit everything 
+    params.require(:post).permit(:title, :url, :description)
   end
 end
