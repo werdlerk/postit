63726142c1b01370deb754feed6c28b937fb87f6
Koen Werdler <koen@morgenwerk.nl>
2014-10-08 21:17:43 +0200

    Added code in Voteable module to show normal metaprogramming (without Rails) of including methods in a module

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -25,3 +25,48 @@ module Voteable
     votes.where(vote: false, creator: user).exists?
   end
 end
+
+
+
+# Using normal ruby metaprogramming
+=begin
+
+module Voteable
+  def self.included(base)
+    base.send(:include, InstanceMethods)
+    base.extend ClassMethods
+    base.class_eval do
+      setup
+    end
+  end
+
+  module InstanceMethods
+    def votes_total
+      @votes_total ||= votes_up - votes_down
+    end
+
+    def votes_up
+      votes.where(vote: true).size
+    end
+
+    def votes_down
+      votes.where(vote: false).size
+    end
+
+    def has_voted_up?(user)
+      votes.where(vote: true, creator: user).exists?
+    end
+
+    def has_voted_down?(user)
+      votes.where(vote: false, creator: user).exists?
+    end
+  end
+
+  module ClassMethods
+    def setup
+      has_many :votes, as: :voteable
+    end
+  end
+
+end
+=end
